9115ee4978f03e57606e2eb57bd08006893be418
Nathan Fenwick <nathanfenwick@rocketmail.com>
2023-04-10 18:44:58 +0100

    Added working server specific song queues

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from slugify import slugify
 from youtubesearchpython import VideosSearch
 from brains import thinking
 from secrets import token
-
+queues = {}
 TOKEN = token  # Replace this with your bots token
 PREFIX = '!'  # You can choose any prefix you like
 
@@ -32,14 +32,17 @@ async def on_ready():
 async def stop(ctx):
     voice_client = ctx.guild.voice_client
 
+    if ctx.guild.id in queues:
+        queues[ctx.guild.id].clear()
+
     if voice_client and voice_client.is_connected():
         if voice_client.is_playing():
             voice_client.stop()
             await asyncio.sleep(1)  # Wait for the player to stop before disconnecting and removing the file
-        await voice_client.disconnect()
-    else:
-        await ctx.send("I'm not connected to a voice channel.")
-    await mp3_cleaner(ctx)
+            await voice_client.disconnect()
+        else:
+            await ctx.send("I'm not connected to a voice channel.")
+            await mp3_cleaner(ctx)
 
 
 @bot.command(name='play')
@@ -57,24 +60,54 @@ async def play(ctx, *, query: str):
         # Search for YouTube link
         query = await search_youtube(query)
 
-    voice_client = await voice_channel.connect()
+    # Add the song to the server-specific queue
+    await add_to_queue(ctx.guild.id, query)
+
+    voice_client = ctx.guild.voice_client
+    if not voice_client or not voice_client.is_playing():
+        await play_next(ctx)
+    else:
+        await ctx.reply(f'Added to queue: {query}')
+
+
+async def play_next(ctx):
+    if ctx.guild.id not in queues or not queues[ctx.guild.id]:
+        await ctx.send("No more songs in the queue.")
+        return
+
+    url = queues[ctx.guild.id].pop(0)
+
+    voice_channel = ctx.author.voice.channel
+    voice_client = ctx.guild.voice_client
+    if not voice_client:
+        voice_client = await voice_channel.connect()
+
     try:
-        player, filename = await YTDLSource.from_url(query, loop=bot.loop, guild_name=ctx.guild.name)
+        player, filename = await YTDLSource.from_url(url, loop=bot.loop, guild_name=ctx.guild.name)
         while not os.path.isfile(filename):
             await asyncio.sleep(1)  # Wait for the file to be downloaded
-        await ctx.reply(f'Now playing: {query}')
+
+        await ctx.reply(f'Now playing: {url}')
         print(filename)
         await ctx.reply(f'File name: {filename}')
-        voice_client.play(player)
+
+        def play_next_song(error):
+            if error:
+                print(f"An error occurred while playing the song: {error}")
+            asyncio.run_coroutine_threadsafe(play_next(ctx), bot.loop)
+
+        voice_client.play(player, after=play_next_song)
+
         while voice_client.is_playing():
             await asyncio.sleep(1)
 
-        await voice_client.disconnect()
     except Exception as e:
         print(e)
         await ctx.reply('https://tenor.com/view/mgs-metal-gear-fucky-wucky-uh-oh-gif-24704061')
         await voice_client.disconnect()
+
     await mp3_cleaner(ctx)
+
 
 
 class YTDLSource(discord.PCMVolumeTransformer):
@@ -100,6 +133,13 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         data, filename = await loop.run_in_executor(None, pytube_extract_info)
         return cls(discord.FFmpegPCMAudio(filename), data=data), filename
+
+
+async def add_to_queue(guild_id, url):
+    if guild_id in queues:
+        queues[guild_id].append(url)
+    else:
+        queues[guild_id] = [url]
 
 
 async def search_youtube(query):
